3c338650a5e5d7f4d92a4b98889cce78f552c8fd
Seryph <Chrono.hi@gmail.com>
2019-10-14 18:07:06 -0400

    feat: Adds starting code based on the pong tutorial on the kivy website

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MobileGame(Widget):
+    pass
+
+class MobileApp(App):
+    def build(self):
+        return MobileGame()
+
+if __name__=='__main__':
+    MobileGame.run()
